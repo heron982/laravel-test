642fe162ba4f62591378cc65de623dd11dd64b9b
felipehorizonte <felipedevlp@gmail.com>
2023-06-23 08:39:24 -0300

    Teste Endpoint Register passando.

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,20 +2,32 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class EndpointRegister extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_endpoint_cadastro() : void
     {
-        $response = $this->get('/');
+        $user = User::factory()->make();
 
-        $response->assertStatus(200);
+        $response = $this->withHeaders([
+            'Accept' => 'application/json'
+        ])->post('/api/register', [
+            'name'      => $user->name,
+            'email'     => $user->email,
+            'password'  => $user->password
+        ]);
+
+        $response->assertStatus(200)
+            ->assertJson([
+                'status' => 'success'
+            ]);
     }
 }
